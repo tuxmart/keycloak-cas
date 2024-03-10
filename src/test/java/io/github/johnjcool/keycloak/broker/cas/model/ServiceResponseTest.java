76767456b0e8733d976f46e0af57eade82956c03
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-03-10 15:04:25 -0400

    Remove references to dependencies removed upstream

--- src/test/java/io/github/johnjcool/keycloak/broker/cas/model/ServiceResponseTest.java
+++ src/test/java/io/github/johnjcool/keycloak/broker/cas/model/ServiceResponseTest.java
@@ -1,7 +1,5 @@
 package io.github.johnjcool.keycloak.broker.cas.model;
 
-import io.github.johnjcool.keycloak.broker.cas.jaxb.ServiceResponseJaxbContextResolver;
-import io.github.johnjcool.keycloak.broker.cas.jaxb.ServiceResponseJaxbProvider;
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
 import jakarta.ws.rs.client.Client;
@@ -10,11 +8,11 @@ import jakarta.ws.rs.core.Response;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
-import java.io.File;
 import java.io.StringReader;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Collections;
-import org.apache.commons.io.FileUtils;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.junit.AfterClass;
@@ -27,7 +25,7 @@ public class ServiceResponseTest {
   private static Undertow server;
 
   @Test
-  public void testReadWithAttributes() {
+  public void testReadWithAttributes() throws JAXBException {
     Client client = ResteasyClientBuilder.newClient(ResteasyProviderFactory.getInstance());
     WebTarget target =
         client.target(String.format("http://%s:%d%s", "localhost", 9999, "/with-attributes"));
@@ -35,7 +33,17 @@ public class ServiceResponseTest {
     Assert.assertEquals(200, response.getStatus());
     response.bufferEntity();
 
-    ServiceResponse serviceResponse = response.readEntity(ServiceResponse.class);
+    String stringResponse = response.readEntity(String.class);
+
+    Unmarshaller um = JAXBContext.newInstance(ServiceResponse.class).createUnmarshaller();
+    um.setEventHandler(new jakarta.xml.bind.helpers.DefaultValidationEventHandler());
+
+    ServiceResponse serviceResponse =
+        (ServiceResponse) um.unmarshal(new StringReader(stringResponse));
+
+    System.out.println(serviceResponse);
+    System.out.println(serviceResponse.getSuccess());
+    System.out.println(serviceResponse.getSuccess().getAttributes());
     Success success = serviceResponse.getSuccess();
 
     Assert.assertEquals("test", success.getUser());
@@ -81,7 +89,7 @@ public class ServiceResponseTest {
   }
 
   @Test
-  public void testReadWithoutAttributes() {
+  public void testReadWithoutAttributes() throws JAXBException {
     Client client = ResteasyClientBuilder.newClient(ResteasyProviderFactory.getInstance());
     WebTarget target =
         client.target(String.format("http://%s:%d%s", "localhost", 9999, "/without-attributes"));
@@ -89,9 +97,17 @@ public class ServiceResponseTest {
     Assert.assertEquals(200, response.getStatus());
     response.bufferEntity();
 
-    System.out.println(response.readEntity(String.class));
+    String stringResponse = response.readEntity(String.class);
 
-    ServiceResponse serviceResponse = response.readEntity(ServiceResponse.class);
+    Unmarshaller um = JAXBContext.newInstance(ServiceResponse.class).createUnmarshaller();
+    um.setEventHandler(new jakarta.xml.bind.helpers.DefaultValidationEventHandler());
+
+    ServiceResponse serviceResponse =
+        (ServiceResponse) um.unmarshal(new StringReader(stringResponse));
+
+    System.out.println(serviceResponse);
+    System.out.println(serviceResponse.getSuccess());
+    System.out.println(serviceResponse.getSuccess().getAttributes());
     Success success = serviceResponse.getSuccess();
 
     Assert.assertEquals("test", success.getUser());
@@ -100,10 +116,6 @@ public class ServiceResponseTest {
 
   @BeforeClass
   public static void init() {
-    ResteasyProviderFactory.getInstance().registerProvider(ServiceResponseJaxbProvider.class, true);
-    ResteasyProviderFactory.getInstance()
-        .registerProvider(ServiceResponseJaxbContextResolver.class, true);
-
     server =
         Undertow.builder()
             .addHttpListener(9999, "localhost")
@@ -117,9 +129,16 @@ public class ServiceResponseTest {
                       httpServerExchange
                           .getResponseSender()
                           .send(
-                              FileUtils.readFileToString(
-                                  new File("src/test/resources/test-without-attributes.xml"),
-                                  "UTF-8"));
+                              Charset.defaultCharset()
+                                  .encode(
+                                      CharBuffer.wrap(
+                                          """
+<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+    <cas:authenticationSuccess>
+        <cas:user>test</cas:user>
+    </cas:authenticationSuccess>
+</cas:serviceResponse>
+                            """)));
                       break;
                     case "/with-attributes":
                       httpServerExchange
@@ -128,9 +147,29 @@ public class ServiceResponseTest {
                       httpServerExchange
                           .getResponseSender()
                           .send(
-                              FileUtils.readFileToString(
-                                  new File("src/test/resources/test-with-attributes.xml"),
-                                  "UTF-8"));
+                              Charset.defaultCharset()
+                                  .encode(
+                                      CharBuffer.wrap(
+                                          """
+<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+    <cas:authenticationSuccess>
+        <cas:user>test</cas:user>
+        <!-- Begin Ldap Attributes -->
+        <cas:attributes>
+
+                <cas:mail>test.test@test.test</cas:mail>
+
+                <cas:sn>tets</cas:sn>
+
+                <cas:cn>test</cas:cn>
+
+        </cas:attributes>
+        <!-- End Ldap Attributes -->
+
+
+    </cas:authenticationSuccess>
+</cas:serviceResponse>
+                            """)));
                       break;
                     case "/with-multival-attributes":
                       httpServerExchange
@@ -139,10 +178,32 @@ public class ServiceResponseTest {
                       httpServerExchange
                           .getResponseSender()
                           .send(
-                              FileUtils.readFileToString(
-                                  new File(
-                                      "src/test/resources/test-with-multivalue-attributes.xml"),
-                                  "UTF-8"));
+                              Charset.defaultCharset()
+                                  .encode(
+                                      CharBuffer.wrap(
+                                          """
+<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+    <cas:authenticationSuccess>
+        <cas:user>test</cas:user>
+        <!-- Begin Ldap Attributes -->
+        <cas:attributes>
+
+            <cas:mail>test.test@test.test</cas:mail>
+
+            <cas:sn>tets</cas:sn>
+
+            <cas:cn>test</cas:cn>
+
+            <cas:successfulAuthenticationHandlers>LdapAuthenticationHandler</cas:successfulAuthenticationHandlers>
+            <cas:successfulAuthenticationHandlers>mfa-duo</cas:successfulAuthenticationHandlers>
+
+        </cas:attributes>
+        <!-- End Ldap Attributes -->
+
+
+    </cas:authenticationSuccess>
+</cas:serviceResponse>
+                                  """)));
                       break;
                     default:
                       throw new IllegalStateException(
